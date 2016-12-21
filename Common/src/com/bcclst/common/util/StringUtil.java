9dd0ff4b51e992b7e89eaa638f826e6716aa5664
biciclista <biciclista@gmail.com>
2016-12-21 13:05:21 +0100

    Initial commit.

---
+++ Common/src/com/bcclst/common/util/StringUtil.java
@@ -0,0 +1,13 @@
+package com.bcclst.common.util;
+
+public class StringUtil {
+	public static String createStringWithLength(int length) {
+        StringBuilder builder = new StringBuilder();
+ 
+        for (int index = 0; index < length; index++) {
+            builder.append("a");
+        }
+ 
+        return builder.toString();
+    }
+}
